d62f7fc0a2036da425d2cbd8dd8f596e4bc367d5
Jeffrey Lonzanida <111095026+ifejinity@users.noreply.github.com>
2023-03-14 20:13:07 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -132,3 +132,22 @@ document.addEventListener('click', function (e) {
         captionText.innerHTML = img.alt;
     }
 });
+
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "106087010816930");
+chatbox.setAttribute("attribution", "biz_inbox");
+    
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v16.0'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
